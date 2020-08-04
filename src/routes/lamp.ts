a05ff415b01e0ed37b97c914a2fd9a9f3f3f2f4f
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2020-08-04 12:47:38 +0200

    added final lamp companion

--- src/routes/lamp.ts
+++ src/routes/lamp.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import request from "request";
 
 var router = express.Router();
 
 const API_KEY: string = process.env.API_KEY || "key";
+
+const NODE_RED_URL: string =
+  process.env.NODE_RED_URL || "http://localhost:1880/endpoint/desklamp/toggle";
+
+const NODE_RED_USER: string = process.env.NODE_RED_USER || "geheim";
+const NODE_RED_PASS: string = process.env.NODE_RED_PASS || "geheim";
 
 let lampState = false;
 
@@ -35,6 +42,7 @@ const providesKey = (
 // called from node red with an api key
 router.post("/set-state", providesKey, (req, res) => {
   const { state } = req.body;
+  console.log(`setting the state to ${state}`);
   lampState = state;
 
   res.json({ success: true, lampState: lampState });
@@ -47,10 +55,22 @@ router.get("/state", (req, res) => {
   });
 });
 
-router.post("/toggle", (req, res) => {
-  // call nr here and wait for return
-  lampState = !lampState;
-  res.json({ success: true, lampState: lampState });
+router.post("/toggle", async (req, res) => {
+  console.log(`toggling lamp state`);
+  request.post(
+    NODE_RED_URL,
+    {
+      auth: {
+        user: NODE_RED_USER,
+        pass: NODE_RED_PASS,
+        sendImmediately: true,
+      },
+    },
+    () => {
+      lampState = !lampState;
+      res.json({ success: true, lampState: lampState });
+    }
+  );
 });
 
 export default router;
